44c74ea6f0a206e49ea00c3092e71d741bd216b7
azimjnovislom77 <azimjonovislomjon77@gmail.com>
2025-10-14 10:41:52 +0500

    done

--- config/settings.py
+++ config/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-yg)%b2@ph4oebvyp*u4l^nk#9b7nurvkq%of$^ib#m@@jjit@m'
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "*"]
 #
